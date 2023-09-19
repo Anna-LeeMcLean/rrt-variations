aadf678badd514d27101752a7b722c503de1d56c
Anna-Lee McLean <annalee.mclean@libangsurgical.com>
2023-09-19 15:29:02 -0700

    Initial implementation of Node class to replace tuples.

---
+++ node.py
@@ -0,0 +1,8 @@
+
+class Node():
+
+    def __init__(self, x: float, y: float, parent = None, child = None) -> None:
+        self.x = x
+        self.y = y
+        self.parent = parent
+        self.child = child
