9e90e660e22336ee0ce20705962d1f46ce4a7b2f
Anna-Lee McLean <mcleanannalee@gmail.com>
2023-11-22 12:54:57 -0800

    Started break down of rewire_path method. Created two smaller methods.

--- rrt-star.py
+++ rrt-star.py
@@ -13,10 +13,13 @@ class RRTStar():
 
     _success: bool = False
 
-    def __init__(self, start: Node, goal: Node, step_size_mm: float = 20, neighbours: int = 3) -> None:
+    def __init__(self, start: Node, goal: Node, step_size_mm: float = 20, 
+                 neighbours: int = 3, number_of_added_nodes = 50) -> None:
+        
         self.rrt = RRT(start, goal, step_size_mm)
 
         self.neighbours = neighbours
+        self.number_of_added_nodes = number_of_added_nodes
         
 
     def create(self):
@@ -48,44 +51,21 @@ class RRTStar():
 
         6. remove previous two edges and redraw new edges to child/parent
 
-        Number of samples to add = Number of nodes in path * 100?
         '''
 
-        number_of_nodes_to_add = len(self.rrt.path_to_goal.nodes)*10
         count = 0
 
-        while count != number_of_nodes_to_add:
+        while count != self.number_of_added_nodes:
 
             path = self.rrt.path_to_goal.nodes[1:-1]      # remove start and goal nodes from path
-
-            # Step 1: create a random sample in the workspace that is close to a node in the path
             node = random.choice(path)       # choose a node in the path (excluding the start and goal nodes)
 
-            random_x = random.uniform(-self.rrt.step_size_mm, self.rrt.step_size_mm)
-            random_y = random.uniform(-self.rrt.step_size_mm, self.rrt.step_size_mm)
+            # Step 1: create a random sample in the workspace that is close to a node in the path
+            rewire_node = self.sample_space(node=node)
 
-            rewire_node = RewireNode(x=node.x + random_x, y=node.y + random_y)
-            plt.plot(rewire_node.x, rewire_node.y, "rx")
             # Step 2: get n closest neighbours to the random node
-
-            # Initialize a list which will hold the n smallest distances between the random sample and neighbour nodes
-            distances = [math.inf for _ in range(self.neighbours)]   
-            # Initalize a list which will hold the nearest neighbours   
-            neighbour_nodes = [None for _ in range(self.neighbours)]
-
-            for path_node in path:
-                # check distance between path_node and random node
-                dist = rewire_node.euclidean_dist(path_node)
-                max_dist = max(distances)                   # get the current largest distances in the distances list
-
-                if dist < max_dist:
-                    index_of_max_distance = distances.index(max_dist)
-                    distances[index_of_max_distance] = dist     # replace current largest distance with new distance
-                    neighbour_nodes[index_of_max_distance] = path_node    # replace corresponding node with new node
-
-            # The nodes list now holds the n closest path nodes to our random sample. These are the random samples neighbours.
-            # The distances list holds the corresponding n smallest distances.
-
+            neighbour_nodes = self.find_n_nearest_neighbours(rewire_node, path)
+            
             # Step 3: for each neighbour, if the cost to the neighbour's parent is less through the 
             # random sample than it currently is, rewire the path.
             largest_cost_difference = -math.inf
@@ -126,6 +106,38 @@ class RRTStar():
                 self.rrt.path_to_goal.plot()
 
             count+=1
+
+    def sample_space(self, node) -> Node:
+
+        random_x = random.uniform(-self.rrt.step_size_mm, self.rrt.step_size_mm)
+        random_y = random.uniform(-self.rrt.step_size_mm, self.rrt.step_size_mm)
+
+        rewire_node = RewireNode(x=node.x + random_x, y=node.y + random_y)
+        plt.plot(rewire_node.x, rewire_node.y, "rx")
+
+        return rewire_node
+
+    def find_n_nearest_neighbours(self, rewire_node: Node, path: list[Node]) -> list[Node]:
+            
+        # Initialize a list which will hold the n smallest distances between the random sample and neighbour nodes
+        distances = [math.inf for _ in range(self.neighbours)]   
+        # Initalize a list which will hold the nearest neighbours   
+        neighbour_nodes = [None for _ in range(self.neighbours)]
+
+        for path_node in path:
+            # check distance between path_node and random node
+            dist = rewire_node.euclidean_dist(path_node)
+            max_dist = max(distances)                   # get the current largest distances in the distances list
+
+            if dist < max_dist:
+                index_of_max_distance = distances.index(max_dist)
+                distances[index_of_max_distance] = dist     # replace current largest distance with new distance
+                neighbour_nodes[index_of_max_distance] = path_node    # replace corresponding node with new node
+
+        # The nodes list now holds the n closest path nodes to our random sample. These are the random samples neighbours.
+        # The distances list holds the corresponding n smallest distances.
+        
+        return neighbour_nodes
 
 
 start1 = Node(x=10.0, y=10.0)
